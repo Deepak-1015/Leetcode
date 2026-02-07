92def2b827a593eaa8baf9ed1701a56850dde73d
Deepak <deepak5441828@gmail.com>
2026-02-07 14:26:20 +0530

    Time: 7 ms (65.61%), Space: 46.6 MB (76.78%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        Set<Character> hs=new HashSet<>();
+
+        for(char c:s.toCharArray()){
+            hs.add(c);
+        }
+        return hs.size();
+    }
+}
